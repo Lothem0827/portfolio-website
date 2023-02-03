d28471de1021590af1e81165333f757e4f10fec5
Lothem0827 <carolino.villartajr@gmail.com>
2023-02-03 12:28:08 +0800

    React Boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Home from "./assets/components/Home/Home";
+
+export const App = () => {
+  return (
+    <div className=" ">
+      <Home />
+    </div>
+  );
+};
---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <>
+      <div className=" bg-red-500"> asdasdasdasdasdsad</div>
+    </>
+  );
+}
+
+export default Home;
